874b7cc92c1de0000898a2bd3c804f67316255b2
acsbendi <acsbendi28@gmail.com>
2019-02-13 21:34:10 +0100

    Implemented downloading ndk

---
+++ download-ndk-and-build-sfml.bat
@@ -0,0 +1,36 @@
+@echo off
+set latest_ndk_version=19
+set android_zip_name=android-ndk-r%latest_ndk_version%-windows-x86_64.zip
+IF [%1] == [] (
+	set ndk_download_path=%~dp0
+) ELSE (
+	IF "%1:~0,1%" == "." (
+		set ndk_download_path=%~dp0\%
+	) ELSE (
+		set ndk_download_path=%1
+	)
+)
+
+wget --directory-prefix %ndk_download_path% https://dl.google.com/android/repository/%android_zip_name%
+
+Call :UnZipFile "%ndk_download_path%" "%ndk_download_path%android-ndk-r%latest_ndk_version%-windows-x86_64.zip"
+del %ndk_download_path%%android_zip_name%
+
+build-sfml.bat %ndk_download_path%android-ndk-r%latest_ndk_version%
+
+exit /b
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
