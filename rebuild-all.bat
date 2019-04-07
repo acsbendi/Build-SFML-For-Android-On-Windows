a93a0f4fdff4e9359be65430566b0a3fc069cc72
Bendegúz Ács <30595431+acsbendi@users.noreply.github.com>
2019-04-08 01:15:02 +0200

    Removed unused argument for rebuild.bat

--- rebuild-all.bat
+++ rebuild-all.bat
@@ -3,6 +3,6 @@ REM 64 bit abis (arm64-v8a and x86_64) currently fail due to not finding OpenAL,
 set abis=x86 armeabi-v7a
 (for %%a in (%abis%) do ( 
    pushd %~dp0%%a
-   start rebuild.bat 19
+   start rebuild.bat
    popd
 ))
