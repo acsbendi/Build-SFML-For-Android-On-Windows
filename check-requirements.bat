baae686c411cf26736c4586fa2b56bdb5eaa65e9
Bendegúz Ács <30595431+acsbendi@users.noreply.github.com>
2019-04-08 00:30:03 +0200

    Fixed requirements checking

--- check-requirements.bat
+++ check-requirements.bat
@@ -3,9 +3,9 @@
 set requirements=cmake git
 
 (for %%r in (%requirements%) do ( 
-    	where cmake.exe >nul 2>nul
+    	where %%r.exe >nul 2>nul
     	IF ERRORLEVEL 1 (
-    		echo %requirements%.exe not found in path.
+    		echo %%r.exe not found in path.
     		exit /b 3
 	)
 ))
