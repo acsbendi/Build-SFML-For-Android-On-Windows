129ccb6d8084bf5286eeb2b1a13e623a04c4a103
huberti1 <hubertmisiewicz1@gmail.com>
2021-07-21 15:08:49 +0200

    Removed unknown argument

--- rebuild.bat
+++ rebuild.bat
@@ -7,7 +7,7 @@ del Makefile
 del install*
 del cmake*
 
-cmake -DANDROID_ABI=%current_abi% -DANDROID_PLATFORM=android-21 -stdlib=libc++ -DCMAKE_TOOLCHAIN_FILE=%ndk_path%/build/cmake/android.toolchain.cmake -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=%ndk_path% -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" ../..
+cmake -DANDROID_ABI=%current_abi% -DANDROID_PLATFORM=android-21 -DCMAKE_TOOLCHAIN_FILE=%ndk_path%/build/cmake/android.toolchain.cmake -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=%ndk_path% -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" ../..
 %ndk_path%\prebuilt\windows-x86_64\bin\make.exe
 %ndk_path%\prebuilt\windows-x86_64\bin\make.exe install
 
